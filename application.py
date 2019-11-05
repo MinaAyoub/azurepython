8bd7007819a7cc66acc4e8c16538c2ebfe83d40e
Mina ayoub <minaayoub@gmail.com>
2019-11-04 23:16:24 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-###@app.route("/")
+app.route("/")
 def hello():
     return "Hello Worlds, yeeeekoe!"
 
+app.route("/")
 def itir():
     return "weee"
